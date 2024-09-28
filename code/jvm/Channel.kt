f1fbbdbb1204390d6e8dad35123a4d868638ccb3
Gabriel <gabs8b@gmail.com>
2024-09-28 17:28:19 +0100

    Channel domain

---
+++ code/jvm/Channel.kt
@@ -0,0 +1,27 @@
+package pt.isel.intro
+
+import java.util.*
+import kotlin.collections.List
+
+const val MAX_DESCRIPTION_LENGTH = 400
+
+data class Channel (
+    val id : UUID,
+    val name : String,
+    val participants : Set<UUID>,
+    val admins : Set<UUID>,
+    val description : String,
+    val message: List<Message>,
+    val visibility: Visibility
+){
+    init {
+        require(name.length in MIN_NAME_LENGTH  .. MAX_NAME_LENGTH &&
+                name.isNotBlank() &&
+                description.length < MAX_DESCRIPTION_LENGTH
+        )
+    }
+}
+
+enum class Visibility{
+    PUBLIC, PRIVATE
+}
