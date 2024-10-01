b76385eddef12191a45a13648e6286fcf88cc687
adriano9358 <tiagoadriano9358@gmail.com>
2024-10-01 10:16:23 +0100

    Project structure refactor

---
+++ code/jvm/repository/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+}
+
+group = "pt.isel"
+version = "0.0.1-SNAPSHOT"
+
+kotlin {
+    jvmToolchain(17)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
