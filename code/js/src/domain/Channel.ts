6dc8446bb3032164757cb8b399e04bc352fef0ef
adriano9358 <tiagoadriano9358@gmail.com>
2024-12-02 11:54:28 +0000

    Further developments

---
+++ code/js/src/domain/Channel.ts
@@ -0,0 +1,10 @@
+
+export type Visibility = "PUBLIC" | "PRIVATE";
+
+export type Channel = {
+    id: number;  
+    name: string;
+    adminID: number;
+    description: string;
+    visibility: Visibility;
+}
